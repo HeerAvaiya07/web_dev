f1ec2d25102e817bc14a36d4e1c1cb124542dc31
HeerAvaiya07 <144682636+HeerAvaiya07@users.noreply.github.com>
2025-05-27 16:49:24 +0530

    tea-app

--- BACKEND/02_express/index.js
+++ BACKEND/02_express/index.js
@@ -3,17 +3,50 @@ import express from 'express';
 const app = express();
 const port = 3000
 
-app.get("/", (req, res) => {
-    res.send("hello from heer avaiya");
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+app.post('/teas', (req, res) => {
+    const { name, price } = req.body;
+    const newTea = { id: nextId++, name, price }
+    teaData.push(newTea);
+    res.status(201).send(newTea);
 })
 
-app.get("/ice-tea", (req, res) => {
-    res.send("what ice tea would you prefer?");
+app.get('/teas', (req, res) => {
+    res.status(200).send(teaData);
 })
 
-app.get("/twitter", (req, res) => {
-    res.send("heerdotcom");
+app.get('/teas/:id', (req, res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+    if (!tea) {
+        return res.status(404).send('Tea not found')
+    }
+    res.status(200).send(tea);
 })
+
+app.put('/teas/:id', (req, res) => {
+    const tea = teaData.find(tea => tea.id === parseInt(req.params.id))
+    if (!tea) {
+        return res.status(404).send('Tea not found')
+    }
+    const { name, price } = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)
+})
+
+app.delete("/teas/:id", (req, res) => {
+    const index = teaData.findIndex((tea) => tea.id === parseInt(req.params.id));
+    if (index === -1) {
+        return res.status(404).send("Tea not found");
+    }
+
+    teaData.splice(index, 1);
+    res.status(204).send("Deleted");
+});
 
 app.listen(port, () => {
     console.log(`Server is running at port:${port}...`)
